e36bf18847a5415348196deec5f9385d2bca96b8
Anand-9534 <67726189+Anand-9534@users.noreply.github.com>
2021-01-10 02:07:59 +0530

    second push

---
+++ component/Card/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from '../../styles/Card.module.css';
+
+function Card(props) {
+    return (
+        <div className={styles.card}>
+            <div className={styles.cardimage}> 
+            <div className={styles.promo}> promoted </div>
+            <img src="/logo_sample.png" alt="" />
+            
+            </div>
+            <div className={styles.cardcontent}>
+                <h4>itemname</h4> 
+            </div>
+            <div className={styles.cardfooter}>
+            <h4>$ 200 </h4> <div className={styles.addbutton}>add</div>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
